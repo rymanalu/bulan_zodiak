e4b7220d337e4940951548d37b562a54e6e7c696
Roni Yusuf Manalu <rymanalu@gmail.com>
2016-10-02 21:44:17 +0700

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Tugas Algoritma dan Pemrograman 1: Program untuk Menentukan Nama Bulan dan Zodiak berdasarkan Inputan
+
+## Dibuat oleh: Roni Yusuf
+
+## NPM: 0616104045
+
+## Universitas Widyatama
---
+++ script.cpp
@@ -0,0 +1,98 @@
+/*
+ | Nama  : Roni Yusuf
+ | NPM   : 0616104045
+ |
+ | Program Penentuan Nama Bulan dan Zodiak
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/**
+ * Returns month's name by given month.
+ *
+ * @param  int  month
+ * @return string
+ */
+string guess_month(int month)
+{
+    if (month < 1 || month > 12) {
+        return "Nomor bulan tidak boleh kurang dari 1 atau lebih dari 12!";
+    }
+
+    string months[12] = {"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
+
+    return months[month - 1];
+}
+
+/**
+ * Returns zodiac by given date.
+ *
+ * @param  int  month
+ * @param  int  date
+ * @return string
+ */
+string guess_zodiac(int month, int date)
+{
+    if (month < 1 || month > 12) {
+        return "Nomor bulan tidak boleh kurang dari 1 atau lebih dari 12!";
+    }
+
+    --month;
+
+    int zodiac_date_separator[12] = {19, 18, 20, 19, 20, 20, 22, 22, 22, 22, 21, 21};
+
+    string zodiacs[12][2] = {
+        {"Capricorn", "Aquarius"},
+        {"Aquarius", "Pisces"},
+        {"Pisces", "Aries"},
+        {"Aries", "Taurus"},
+        {"Taurus", "Gemini"},
+        {"Gemini", "Cancer"},
+        {"Cancer", "Leo"},
+        {"Leo", "Virgo"},
+        {"Virgo", "Libra"},
+        {"Libra", "Scorpio"},
+        {"Scorpio", "Sagittarius"},
+        {"Sagittarius", "Capricorn"}
+    };
+
+    int separator = zodiac_date_separator[month];
+
+    return date > separator ? zodiacs[month][1] : zodiacs[month][0];
+}
+
+/**
+ * The main program.
+ *
+ * @return int
+ */
+int main()
+{
+    int date;
+    int month;
+
+    cout << "Program Penentuan Nama Bulan dan Zodiak" << endl;
+    cout << "---------------------------------------" << endl;
+    cout << "Dibuat oleh: Roni Yusuf" << endl;
+    cout << "NPM: 0616104045" << endl;
+    cout << "Universitas Widyatama" << endl;
+    cout << "---------------------------------------" << endl;
+
+    cout << "Input tanggal (contoh: 7):" << endl;
+    cin >> date;
+
+    cout << endl;
+
+    cout << "Input nomor bulan (contoh: 3):" << endl;
+    cin >> month;
+
+    cout << endl;
+
+    cout << "Nama bulannya adalah: " << guess_month(month) << endl << endl;
+    cout << "Zodiak-nya adalah: " << guess_zodiac(month, date) << endl;
+
+    return 0;
+}
